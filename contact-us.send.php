eb8091fd0b76db6da307d8a93031405482bca628
amitdnets <47971004+amitdnets@users.noreply.github.com>
2019-03-19 12:04:20 +0530

    Update contact-us.send.php

--- contact-us.send.php
+++ contact-us.send.php
@@ -3,7 +3,7 @@ $name       = @trim(stripslashes($_POST['name']));
 $from       = @trim(stripslashes($_POST['email'])); 
 $subject    = @trim(stripslashes($_POST['subject'])); 
 $message    = @trim(stripslashes($_POST['message'])); 
-$to   		= 'email@email.com'; //Change this var whit your email
+$to   		= 'amitdnets@gmail.com'; //Change this var whit your email
 
 $headers   = array();
 $headers[] = "MIME-Version: 1.0";
